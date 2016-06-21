bb581a3348eb768eb9176c8818e9f38a6ee1f51a
Samuel <samuelcarmona123@gmail.com>
2016-06-21 14:51:35 +0200

    Fixed gmail api transport

--- django_mailbox/transports/gmailapi.py
+++ django_mailbox/transports/gmailapi.py
@@ -17,22 +17,23 @@ class GmailAPITransport(EmailTransport):
         self.user_id = user_id
         self.watch_address = watch_address[1:]
 
-        request = { 'labelIds': ['INBOX'],  'topicName': watch_address }
+        self.request = { 'labelIds': ['INBOX'],  'topicName': str(self.watch_address)}
 
-        credentials = server_side_gmail.get_gmail_credentials(user_id)
+        self.credentials = server_side_gmail.get_gmail_credentials(user_id)
 
-        http = credentials.authorize(server_side_gmail.httplib2.Http())
+        self.http = self.credentials.authorize(server_side_gmail.httplib2.Http())
 
-        service = server_side_gmail.discovery.build('gmail', 'v1', http=http)
-        service.users().watch(userId='me', body=request).execute()
+        self.service = server_side_gmail.discovery.build('gmail', 'v1', http=self.http)
+        self.service.users().watch(userId='me', body=self.request).execute()
+
 
     def get_message(self, condition=None):
-        messages_list = service.users().messages().list(userId='me', q="is:unread").execute()
+        messages_list = self.service.users().messages().list(userId='me', q="is:unread").execute()
 
         if "messages" in messages_list:
             for message_gmail in messages_list["messages"]:
                 try:
-                    message_api_gmail = service.users().messages().get(userId='me', id=message_gmail["id"], format='raw').execute()
+                    message_api_gmail = self.service.users().messages().get(userId='me', id=message_gmail["id"], format='raw').execute()
 
                     msg_str = base64.urlsafe_b64decode(message_api_gmail['raw'].encode('utf-8','ignore'))
 
@@ -43,7 +44,7 @@ class GmailAPITransport(EmailTransport):
                     INBOX, SPAM, TRASH, UNREAD, STARRED, IMPORTANT, SENT, Draft
                     """
                     msg_labels = { "addLabelIds": [],"removeLabelIds": ['UNREAD', 'INBOX']}
-                    service.users().messages().modify(userId='me', id=message_gmail["id"],  body=msg_labels).execute()
+                    self.service.users().messages().modify(userId='me', id=message_gmail["id"],  body=msg_labels).execute()
 
                     if condition and not condition(msg):
                         continue
